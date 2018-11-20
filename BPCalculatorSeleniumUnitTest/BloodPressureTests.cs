397424b01c40a6a65ab05f164331e2ef9e1214d2
John McIntyre <john.mcintyre@ericsson.com>
2018-11-20 14:27:56 +0000

    No Jira - Added sample Selenium Tests - again more updates

--- BPCalculatorSeleniumUnitTest/BloodPressureTests.cs
+++ BPCalculatorSeleniumUnitTest/BloodPressureTests.cs
@@ -58,7 +58,7 @@ namespace BPCalculatorSeleniumUnitTest
             switch (browser)
             {
                 case "Chrome":
-                    driver = new ChromeDriver();
+                    driver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"));
                     break;
                 case "Firefox":
                     driver = new FirefoxDriver();
